731e7c7fb709e73c513eaad5e6dd984ac03f5e08
SINA PANAHI <93608711+S1NAPANAHI@users.noreply.github.com>
2025-10-04 19:29:23 +0200

    Create basic Room database to prevent crashes

---
+++ app/src/main/java/com/example/zoroastervers/data/local/database/ZoroasterDatabase.kt
@@ -0,0 +1,17 @@
+package com.example.zoroastervers.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+/**
+ * Room database for ZoroasterVers app
+ * Currently empty, but ready for future entities
+ */
+@Database(
+    entities = [],
+    version = 1,
+    exportSchema = false
+)
+abstract class ZoroasterDatabase : RoomDatabase() {
+    // DAOs will be added here as entities are created
+}
